a071e8a4283c8545a8f720fac02fc57db6337692
ehoummad <enzo.hoummady@insa-cvl.fr>
2022-10-04 16:04:22 -0400

    des truc je sais plus

--- TP2.py
+++ TP2.py
@@ -1,4 +1,5 @@
 
+from distutils.util import copydir_run_2to3
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,7 +15,6 @@ import csv
 # Chargement des données
 data = csv.reader(open("SeoulBikedata.csv", "r"),
                   delimiter=",")
-
 
 data = list(data)
 # delete la premiere ligne
@@ -69,6 +69,22 @@ for i in range(0, int(len(data))):
     elif data[i][11] == 'Autumn':
         data[i][11] = 3
 
+data = data.astype(numpy.float)
+
+# delete all line of data where the value of last column is 0
+tmp_data = data
+for i in range(0, int(len(data))):
+    if data[i][13] == 0:
+        tmp_data = numpy.delete(data, i, 0)
+
+data = tmp_data
+
+# keep only the line where the value of column 11 is 4
+winter_data = data
+for i in range(0, int(len(data))):
+    if data[i][11] != 4:
+        winter_data = numpy.delete(data, i, 0)
+
 
 pourcentage = 0.7
 
@@ -80,48 +96,34 @@ def split_data(data, pourcentage):
 
 
 train, test = split_data(data, pourcentage)
+train_winter, test_winter = split_data(winter_data, pourcentage)
 
 # extract second column as target both for train and test
 target_train = [i[1] for i in train]
 target_test = [i[1] for i in test]
 
+target_train_winter = [i[1] for i in train_winter]
+target_test_winter = [i[1] for i in test_winter]
+
 # delete second column from train and test
 train = numpy.delete(train, 1, 1)
 test = numpy.delete(test, 1, 1)
+train_winter = numpy.delete(train_winter, 1, 1)
+test_winter = numpy.delete(test_winter, 1, 1)
+# delete last column from train and test
+train = numpy.delete(train, 11, 1)
+test = numpy.delete(test, 11, 1)
+train = numpy.delete(train, 11, 1)
+test = numpy.delete(test, 11, 1)
+train = numpy.delete(train, 7, 1)
+test = numpy.delete(test, 7, 1)
 
 
 reg = LinearRegression().fit(train, target_train)
+reg2 = LinearRegression().fit(train_winter, target_train_winter)
 
-"""def mse(coef, x, y):
-    return np.mean((y - np.dot(x, coef))**2)
+y_prediction = reg.predict(test)
+print(y_prediction)
 
-
-def gradients(coef, x, y):
-    return -2 * np.dot(x.T, y - np.dot(x, coef)) / y.size
-
-
-def multilinear_regression(coef, x, y, lr, b1=0.9, b2=0.999, epsilon=1e-8):
-    prev_error = 0
-    m_coef = np.zeros(coef.shape)
-    v_coef = np.zeros(coef.shape)
-    moment_m_coef = np.zeros(coef.shape)
-    moment_v_coef = np.zeros(coef.shape)
-    t = 0
-
-    while True:
-        error = mse(coef, x, y)
-        if abs(error - prev_error) <= epsilon:
-            break
-        prev_error = error
-        grad = gradients(coef, x, y)
-        t += 1
-        m_coef = b1 * m_coef + (1-b1)*grad
-        v_coef = b2 * v_coef + (1-b2)*grad**2
-        moment_m_coef = m_coef / (1-b1**t)
-        moment_v_coef = v_coef / (1-b2**t)
-
-        delta = ((lr / moment_v_coef**0.5 + 1e-8) *
-                 (b1 * moment_m_coef + (1-b1)*grad/(1-b1**t)))
-
-        coef = np.subtract(coef, delta)
-    return coef"""
+print(reg)
+print(reg2.score(test_winter, target_test_winter))
